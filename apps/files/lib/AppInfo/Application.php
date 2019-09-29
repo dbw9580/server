496af1620000da86bd678d7409de179bc547437b
Daniel Kesselberg <mail@danielkesselberg.de>
2019-09-29 20:57:00 +0200

    Decouple resource provider registration
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- apps/files/lib/AppInfo/Application.php
+++ apps/files/lib/AppInfo/Application.php
@@ -38,7 +38,7 @@ use OCA\Files\Listener\LegacyLoadAdditionalScriptsAdapter;
 use OCA\Files\Notification\Notifier;
 use OCA\Files\Service\TagService;
 use OCP\AppFramework\App;
-use OCP\Collaboration\Resources\IManager;
+use OCP\Collaboration\Resources\IProviderManager;
 use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IContainer;
 
@@ -89,9 +89,9 @@ class Application extends App {
 		/**
 		 * Register Collaboration ResourceProvider
 		 */
-		/** @var IManager $resourceManager */
-		$resourceManager = $container->query(IManager::class);
-		$resourceManager->registerResourceProvider(ResourceProvider::class);
+		/** @var IProviderManager $providerManager */
+		$providerManager = $container->query(IProviderManager::class);
+		$providerManager->registerResourceProvider(ResourceProvider::class);
 		Listener::register($server->getEventDispatcher());
 
 		/** @var IEventDispatcher $dispatcher */
